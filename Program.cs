6c3f7b4dd33850ab18fe83fd0b637e16d3353b5f
codingcele <marcocelegato9@gmail.com>
2023-04-04 16:04:51 +0200

    bonus

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace cibiPreferiti
     {
         static void Main(string[] args)
         {
-            string[] cibiPreferiti = { "carbonara", "cioccolata", "anacardi", "pizza", "kebab", "poke", "pere" };
+            string[] cibiPreferiti = { "carbonara", "cioccolata", "anacardi", "pizza", "kebab", "poke", "pere", "paella" };
 
             int length = cibiPreferiti.Length;
             Console.WriteLine("Length is: " + length);
@@ -18,6 +18,24 @@ namespace cibiPreferiti
 
             var ultimoCibo = cibiPreferiti[cibiPreferiti.Length - 1];
             Console.WriteLine("Last food in most favourite foods rank is: " + ultimoCibo);
+
+            if (length % 2 == 0)
+            {
+                int stHalf = length / 2 - 1;
+                int ndHalf = length / 2;
+
+                var foodStHalf = cibiPreferiti[stHalf];
+                var foodNdHalf = cibiPreferiti[ndHalf];
+
+                Console.WriteLine("Foods in the middle: " + foodStHalf + " and " + foodNdHalf);
+            }
+            else
+            {
+                Console.WriteLine(length);
+                int half = length / 2;
+                var middleFood = cibiPreferiti[half];
+                Console.WriteLine("The food in the middle is: "+ middleFood);
+            }
         }
     }
 }
